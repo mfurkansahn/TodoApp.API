3a1415c5063d5509e86a68241547c0ca8f2a4c34
mfurkansahn <76617516+mfurkansahn@users.noreply.github.com>
2024-03-26 22:18:09 +0300

    Add project files.

---
+++ TodoApp.API/Controllers/TodoController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TodoApp.API.Data;
+using TodoApp.API.Models;
+
+namespace TodoApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        public readonly TodoDbContext _todoDbContext;
+
+        public TodoController(TodoDbContext todoDbContext)
+        {
+            _todoDbContext = todoDbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllTodos()
+        {
+            var todos = await _todoDbContext.Todos
+                .Where(x => x.IsDeleted == false)
+                .OrderByDescending(x => x.CreatedDate)
+                .ToListAsync();
+
+            return Ok(todos);
+        }
+
+        [HttpGet]
+        [Route("get-deleted-todos")]
+        public async Task<IActionResult> GetAllDeletedTodos()
+        {
+            var todos = await _todoDbContext.Todos
+                .Where(x => x.IsDeleted == true)
+                .OrderByDescending(x => x.CreatedDate)
+                .ToArrayAsync();
+
+            return Ok(todos);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddTodo(Todo todo)
+        {
+            todo.Id = Guid.NewGuid(); //Kullanıcıya otamtik benzersiz(unique) id atansın
+
+            _todoDbContext.Todos.Add(todo); //veritabanına ekle
+            await _todoDbContext.SaveChangesAsync(); //değişiklikleri kaydet
+
+            return Ok(todo);
+        }
+
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> UpdateTodo([FromRoute] Guid id, Todo todoUpdateRequest)
+        {
+            var todo = await _todoDbContext.Todos.FindAsync(id);
+
+            if (todo == null)
+                return NotFound();
+
+
+            todo.IsCompleted = todoUpdateRequest.IsCompleted;
+            todo.CompletedDate = DateTime.Now;
+
+            await _todoDbContext.SaveChangesAsync();
+
+            return Ok(todo);
+        }
+
+        [HttpDelete]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> DeleteTodo([FromRoute] Guid id)
+        {
+            var todo = await _todoDbContext.Todos.FindAsync(id);
+
+            if (todo == null)
+                return NotFound();
+
+            todo.IsDeleted = true;
+            todo.DeletedDate = DateTime.Now;
+
+            await _todoDbContext.SaveChangesAsync();
+
+            return Ok(todo);
+        }
+
+        [HttpPut]
+        [Route("undo-deleted-todo/{id:Guid}")]
+        public async Task<IActionResult> UndoDeletedTodo([FromRoute] Guid id, Todo undoDeleteTodoRequest)
+        {
+            var todo = await _todoDbContext.Todos.FindAsync(id);
+
+            if(todo == null)
+                return NotFound();
+
+            todo.DeletedDate = null;
+            todo.IsDeleted = false;
+
+            await _todoDbContext.SaveChangesAsync();
+
+            return Ok(todo);
+        }
+    }
+}
